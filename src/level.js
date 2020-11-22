30fd3c85a0f31001c59a1cf87f66959def39d071
Erick Santos <eksantos.jr@gmail.com>
2020-11-21 19:43:06 -0800

    Add background rendering

--- src/level.js
+++ src/level.js
@@ -1,6 +1,17 @@
 export default class Level {
     constructor(dimensions){
         this.dimensions = dimensions;
-        
+
+    }
+
+    drawBackground(ctx){
+        //receives canvas context, draws background
+        ctx.fillStyle = "PALETURQUOISE";
+        ctx.fillRect(0, 0, this.dimensions.width, this.dimensions.height);
+    }
+
+    animate(ctx){
+        //receives canvas context, draws structures in the level
+        this.drawBackground(ctx);
     }
 }
