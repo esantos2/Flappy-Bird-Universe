71c41e1fcc48310b7782133d5406d98608caea25
Erick Santos <eksantos.jr@gmail.com>
2020-11-22 13:10:42 -0800

    Add game loop and mouse event listener

--- src/game.js
+++ src/game.js
@@ -11,19 +11,41 @@ export default class Game {
         };
         this.level = null;
         this.bird = null;
+        this.running = false;
+        this.addEvents();
         this.restart();
     }
 
+    click(){
+        //clicking makes bird fly up, or starts the game if not running
+        if (!this.running) this.play();
+        this.bird.flap();
+    }
+
+    addEvents(){
+        //add events to canvas
+        this.ctx.canvas.addEventListener("mousedown", () => this.click());  //mousedown calls click()
+    }
+
     restart(){
+        //reset the level, bird, running status, then call animate
         this.level = new Level(this.dimensions);
         this.bird = new Bird(this.dimensions);
-        
+        this.running = false;
+        this.animate();
+    }
+
+    play(){
+        //begin playing game, set running status and start animation
+        this.running = true;
         this.animate();
     }
 
     animate(){
+        //creates images on canvas while the game is running
         this.level.animate(this.ctx);
         this.bird.animate(this.ctx);
+        if (this.running) requestAnimationFrame(() => this.animate());
     }
 
 }
