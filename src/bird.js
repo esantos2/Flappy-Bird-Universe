cacda1aaeff50993fc1a3a1a67bb3c238eb933e5
Erick Santos <eksantos.jr@gmail.com>
2020-11-22 13:08:41 -0800

    Add bird movement functionality

--- src/bird.js
+++ src/bird.js
@@ -1,7 +1,10 @@
 const CONSTANTS = {
     BIRD_WIDTH: 40,
-    BIRD_HEIGHT: 30
-}
+    BIRD_HEIGHT: 30,
+    FLAP_SPEED: 12,
+    GRAVITY: 0.4,
+    TERMINAL_VELOCITY: 12
+};
 
 export default class Bird {
     constructor(dimensions){
@@ -12,11 +15,33 @@ export default class Bird {
     }
 
     drawBird(ctx){
-        ctx.fillStyle = "yellow";
+        //receives canvas context, draws bird
+        ctx.fillStyle = "gold";
         ctx.fillRect(this.x, this.y, CONSTANTS.BIRD_WIDTH, CONSTANTS.BIRD_HEIGHT);
     }
 
+    moveBird(){
+        //updates bird velocity and position for each frame
+        this.y += this.velocity;            //update vertical position
+        this.velocity += CONSTANTS.GRAVITY  //update acceleration due to gravity
+
+        //prevent bird from exceeding terminal velocity in positive and negative directions
+        if (Math.abs(this.velocity) > CONSTANTS.TERMINAL_VELOCITY){
+            if (this.velocity > 0){
+                this.velocity = CONSTANTS.TERMINAL_VELOCITY;
+            } else {
+                this.velocity = -1 * CONSTANTS.TERMINAL_VELOCITY;
+            }
+        }
+    }
+
+    flap(){
+        //adjusts velocity for bird to fly up
+        this.velocity = -1 * CONSTANTS.FLAP_SPEED;
+    }
+
     animate(ctx){
+        //receives canvas context, animates bird movement and physics
         this.drawBird(ctx);
     }
 }
