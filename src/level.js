3699ea0de946f4750c0dcf524c5d1698d3777a17
Erick Santos <eksantos.jr@gmail.com>
2020-11-22 15:40:20 -0800

    Add collision detection

--- src/level.js
+++ src/level.js
@@ -9,12 +9,30 @@ const CONSTANTS = {
 export default class Level {
     constructor(dimensions){
         this.dimensions = dimensions;
-        const initialSpawn = this.dimensions.width + (60 * PIPE_SPEED);
+        const initialSpawn = this.dimensions.width + (60 * CONSTANTS.PIPE_SPEED);
         this.pipes = [
             this.getRandomPipe(initialSpawn),
             this.getRandomPipe(initialSpawn + CONSTANTS.PIPE_SPACING),
             this.getRandomPipe(initialSpawn + 2 * CONSTANTS.PIPE_SPACING)
         ];
+    }
+
+    collidesWith(birdBounds){
+        //returns true if bird collides with any pipe, false otherwise
+        let hitDetection = false;
+        this.pipes.forEach( pipeSet => {
+            const topHit = this._overlap(pipeSet.topPipe, birdBounds);
+            const bottomHit = this._overlap(pipeSet.bottomPipe, birdBounds);
+            if (topHit || bottomHit) hitDetection = true; 
+        });
+        return hitDetection;
+    }
+
+    _overlap(box1, box2){
+        //returns true if box1 overlaps with box2, false otherwise
+        if (box1.left > box2.right || box1.right < box2.left) return false; //no overlap in x direction
+        if (box1.top > box2.bottom || box1.bottom < box2.top) return false; //no overlap in y direction
+        return true;
     }
 
     getRandomPipe(x){
@@ -28,7 +46,7 @@ export default class Level {
                 top: 0,
                 bottom: gapTop
             },
-            btmPipe: {
+            bottomPipe: {
                 left: x,
                 right: x + CONSTANTS.PIPE_WIDTH,
                 top: gapTop + CONSTANTS.GAP_HEIGHT,
@@ -44,8 +62,8 @@ export default class Level {
         this.pipes.forEach ( pipe => {
             pipe.topPipe.left -= CONSTANTS.PIPE_SPEED;
             pipe.topPipe.right -= CONSTANTS.PIPE_SPEED;
-            pipe.btmPipe.left -= CONSTANTS.PIPE_SPEED;
-            pipe.btmPipe.right -= CONSTANTS.PIPE_SPEED;
+            pipe.bottomPipe.left -= CONSTANTS.PIPE_SPEED;
+            pipe.bottomPipe.right -= CONSTANTS.PIPE_SPEED;
         });
 
         //remove from array when offscreen and add new pipe w/ new gap
