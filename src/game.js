af54911299bfab2a05c8cab303378e5e312c4f81
Erick Santos <eksantos.jr@gmail.com>
2020-11-21 19:51:25 -0800

    Add Bird and Level to Game class

--- src/game.js
+++ src/game.js
@@ -1,3 +1,7 @@
+import Bird from "./bird";
+import Level from "./level";
+
+
 export default class Game {
     constructor(canvas){
         this.ctx = canvas.getContext("2d");
@@ -5,6 +9,21 @@ export default class Game {
             width: canvas.width,
             height: canvas.height
         };
+        this.level = null;
+        this.bird = null;
+        this.restart();
     }
-    
+
+    restart(){
+        this.level = new Level(this.dimensions);
+        this.bird = new Bird(this.dimensions);
+        
+        this.animate();
+    }
+
+    animate(){
+        this.level.animate(this.ctx);
+        this.bird.animate(this.ctx);
+    }
+
 }
