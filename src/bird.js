c170db425d16029201358629f4088ad1cbaa9fa7
Erick Santos <eksantos.jr@gmail.com>
2020-11-21 19:50:57 -0800

    Add functions to draw bird to canvas

--- src/bird.js
+++ src/bird.js
@@ -1,6 +1,22 @@
+const CONSTANTS = {
+    BIRD_WIDTH: 40,
+    BIRD_HEIGHT: 30
+}
+
 export default class Bird {
     constructor(dimensions){
         this.dimensions = dimensions;
-        
+        this.x = this.dimensions.width / 3;
+        this.y = this.dimensions.height / 2;
+        this.velocity = 0;
+    }
+
+    drawBird(ctx){
+        ctx.fillStyle = "yellow";
+        ctx.fillRect(this.x, this.y, CONSTANTS.BIRD_WIDTH, CONSTANTS.BIRD_HEIGHT);
+    }
+
+    animate(ctx){
+        this.drawBird(ctx);
     }
 }
