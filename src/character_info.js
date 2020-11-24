6ada2164e300e0b89637beea6896ba0cdc229686
Erick Santos <eksantos.jr@gmail.com>
2020-11-24 01:31:10 -0800

    Add all character asset info

---
+++ src/character_info.js
@@ -0,0 +1,47 @@
+//all character names
+const FLAPPY_BIRD = "flappyBird";
+const MARIO_FISH = "marioFish";
+const NYAN_CAT = "nyanCat";
+const ANGRY_BIRD = "angryBird";
+const MARIO_CAPE = "marioCape";
+const BANANYA_CAT = "bananyaCat";
+
+//all character image info
+export const allCharInfo = {
+    [FLAPPY_BIRD]: {
+        link: "assets/images/flappyBird.png",     // 51x36
+        width: 51,
+        height: 36,
+        callback: null
+    },
+    [MARIO_FISH]: {
+        link: "assets/images/Fish.png",           // 40x40
+        width: 40,
+        height: 40,
+        callback: null
+    },
+    [NYAN_CAT]: {
+        link: "assets/images/NyanCat.png",        // 65x40
+        width: 65,
+        height: 40,
+        callback: null
+    },
+    [ANGRY_BIRD]: {
+        link: "assets/images/angryBird.png",      // 50x46
+        width: 50,
+        height: 46,
+        callback: null
+    },
+    [MARIO_CAPE]: {
+        link: "assets/images/marioCape.png",      // 56x56
+        width: 56,
+        height: 56,
+        callback: null
+    },
+    [BANANYA_CAT]: {
+        link: "assets/images/Bananya.png",        // 32x55
+        width: 32,
+        height: 55,
+        callback: null
+    }
+}
