31c7a0d943e80bc7ebabc456cbf8863f38f72424
Erick Santos <eksantos.jr@gmail.com>
2020-12-08 16:15:02 -0800

    Add status message for frame counter button

--- src/tools/frame_counter.js
+++ src/tools/frame_counter.js
@@ -1,7 +1,11 @@
 import * as UTILITY from "./utility";
 
 const CONSTANTS = {
-    NAME: "FPS",
+    BUTTON_NAME: "FPS",
+    STATUS_CLASS: "fps-status",
+    SLIDE_EFFECT_CLASS: "show-status",
+    MSG_ON: "Framerate counter on!",
+    MSG_OFF: "Framerate counter off!",
     FPS_CORNER: {
         x: 10,
         y: 20
@@ -12,6 +16,7 @@ export default class FrameCounter {
     constructor(container){
         this.toolbox = container;               //parent container element for all tools
         this.button = this.addFpsButton();      //create and add fps button to toolbox
+        this.statusMessage = this.addStatus();  //create and add element for fps button status
         this.showFPS = false;                   //tracks visibility
         this.startTime = 0;
         this.frameNumber = 0;
@@ -27,10 +32,43 @@ export default class FrameCounter {
     _createFpsButton(){
         //creates and returns button to toggle fps counter
         const newButton = document.createElement("button");
-        newButton.innerHTML = CONSTANTS.NAME;
+        newButton.innerHTML = CONSTANTS.BUTTON_NAME;
+
+        //add event listeners
         newButton.addEventListener("click", this.handleFpsClick());
-        UTILITY.toggleSelectedStatus(newButton);        //add initial styling
+        newButton.addEventListener("mouseover", () => this.toggleStatus());
+        newButton.addEventListener("mouseout", () => this.toggleStatus());
+        
+        //add initial styling
+        UTILITY.toggleSelectedStatus(newButton);
         return newButton;
+    }
+
+    addStatus(){
+        //creates and returns element for fps button status message
+        const newElement = this._createStatusUpdate();
+        this.toolbox.appendChild(newElement);
+        return newElement;
+    }
+
+    _createStatusUpdate(){
+        //creates and returns element 
+        const newElement = document.createElement("div");
+        newElement.setAttribute("class", CONSTANTS.STATUS_CLASS);
+
+        //set initial message
+        newElement.innerHTML = CONSTANTS.MSG_OFF;
+
+        return newElement;
+    }
+
+    toggleStatus(){
+        //updates button status message
+        if (this.statusMessage.id !== CONSTANTS.SLIDE_EFFECT_CLASS){
+            this.statusMessage.setAttribute("id", CONSTANTS.SLIDE_EFFECT_CLASS);
+        } else {
+            this.statusMessage.removeAttribute("id", CONSTANTS.SLIDE_EFFECT_CLASS);
+        }
     }
 
     handleFpsClick(){
@@ -39,7 +77,7 @@ export default class FrameCounter {
             e.preventDefault();
             const fpsButton = e.target;
             this.showFPS = !this.showFPS;
-            
+            this.statusMessage.innerHTML = this.showFPS ? CONSTANTS.MSG_ON : CONSTANTS.MSG_OFF;
             //change styling to indicate button is selected
             UTILITY.toggleSelectedStatus(fpsButton);
         }
