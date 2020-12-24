af34a9cb1f19ccfbe4d0498c3cb44792ca98270d
Erick Santos <eksantos.jr@gmail.com>
2020-12-23 20:30:52 -0800

    Add menu toggle functions

--- src/menus/menu.js
+++ src/menus/menu.js
@@ -2,6 +2,20 @@
 
 class Menu {
     constructor(){
-        
+
     }
+
+    _toggleVisibility(element){
+        //receives reference to element, toggles visibility
+        element.id = (element.id !== "show") ? "show" : "no-show";
+    }
+
+    showMenu(menuElement, titleElement){
+        //receives menu and title elements, toggles background and menu visibility
+        const backBoxElement = document.querySelector(".backBox");
+        [menuElement, titleElement, backBoxElement].forEach( ele => {
+            this._toggleVisibility(ele);
+        });
+    }
+
 }
